32a85b9a3a926862bcdaea18f0b9b46adb0eb495
Abhinay <Abhinay>
2023-09-21 20:59:26 +0530

    Setting up Retrofit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsAPI.kt
@@ -0,0 +1,34 @@
+package com.androiddevs.mvvmnewsapp.api
+
+import com.androiddevs.mvvmnewsapp.model.NewsResponse
+import com.androiddevs.mvvmnewsapp.utils.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Created by Abhinay on 21/09/23.
+ */
+interface NewsAPI {
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+/**
+ * Created by Abhinay on 21/09/23.
+ */
+class Constants {
+    companion object{
+        const val API_KEY = "7abbc226e6894813be81bb4177292f46"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
