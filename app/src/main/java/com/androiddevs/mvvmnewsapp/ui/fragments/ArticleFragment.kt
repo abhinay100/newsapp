7ab5457db53b9772679b4c700bcb9b28ba08a50b
Abhinay <Abhinay>
2023-09-21 20:08:41 +0530

    Setting up Navigation

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+/**
+ * Created by Abhinay on 20/09/23.
+ */
+class ArticleFragment: Fragment(R.layout.fragment_article) {
+}
