b083978289f1a60782b1489fae7d0879657e8020
Abhinay <Abhinay>
2023-09-21 23:16:57 +0530

    Setting up Database and Room type converters

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/Converters.kt
@@ -0,0 +1,20 @@
+package com.androiddevs.mvvmnewsapp.db
+
+import androidx.room.TypeConverter
+import com.androiddevs.mvvmnewsapp.model.Source
+
+
+/**
+* Created by Abhinay on 21/09/23.
+*/class Converters {
+
+    @TypeConverter
+    fun fromSource(source: Source): String {
+        return source.name
+    }
+
+    @TypeConverter
+    fun toSource(name: String): Source {
+        return Source(name, name)
+    }
+}
