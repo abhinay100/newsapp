d65978cff1bb9e70cca46dc47d09a4b6ffa095d1
Abhinay <Abhinay>
2023-09-21 22:33:54 +0530

    DAO creation

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDao.kt
@@ -0,0 +1,24 @@
+package com.androiddevs.mvvmnewsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.androiddevs.mvvmnewsapp.model.Article
+
+/**
+ * Created by Abhinay on 21/09/23.
+ */
+@Dao
+interface ArticleDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
