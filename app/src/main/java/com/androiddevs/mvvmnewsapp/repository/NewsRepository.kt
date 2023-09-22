aa6803d459abbdf40312d2ee958b42ddff0baed1
Abhinay <Abhinay>
2023-09-22 18:55:12 +0530

    Handling Breaking News Response

--- app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
@@ -1,5 +1,6 @@
 package com.androiddevs.mvvmnewsapp.repository
 
+import com.androiddevs.mvvmnewsapp.api.RetrofitInstance
 import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
 
 /**
@@ -8,4 +9,6 @@ import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
 class NewsRepository(
     val db: ArticleDatabase
 ) {
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -1,7 +1,13 @@
 package com.androiddevs.mvvmnewsapp.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevs.mvvmnewsapp.models.NewsResponse
 import com.androiddevs.mvvmnewsapp.repository.NewsRepository
+import com.androiddevs.mvvmnewsapp.utils.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 /**
  * Created by Abhinay on 22/09/23.
@@ -9,4 +15,26 @@ import com.androiddevs.mvvmnewsapp.repository.NewsRepository
 class NewsViewModel(
     val newsRepository: NewsRepository
 ) : ViewModel() {
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val breakingNewsPage = 1
+
+    init {
+        getbreakingNews("us")
+    }
+
+    fun getbreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
+        if(response.isSuccessful) {
+            response.body()?.let {resultResponse ->
+              return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
 }
